2152f55f138f8eccc49d1ce0421729d464d619b1
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-06-25 14:38:38 +0200

    first commit

---
+++ app/Http/Requests/StoreProjectRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'min:3', 'unique:projects'],
+            'content' => ['min:10'],
+            'cover_img' => ['nullable', 'image', 'max:8000'],
+            'type_id' => ['nullable']
+        ];
+    }
+}
---
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'min:3', Rule::unique('projects')->ignore($this->project)],
+            'content' => ['min:10'],
+            'cover_img' => ['nullable', 'image', 'max:8000'],
+            'type_id' => ['nullable']
+        ];
+    }
+}
